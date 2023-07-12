d35f3a32cd05a09d130a5fed75aee1a0cf3bb2cf
linjsh <linjsh@pcl.ac.cn>
2023-07-12 19:52:26 +0800

    add time limit to stop the learning process.

--- app/scheduler.py
+++ app/scheduler.py
@@ -3,6 +3,7 @@ import os
 from sb3_contrib import MaskablePPO
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+import time
 
 from src.agent.encoder import FeaturesExtractor
 from src.env.env_helper import from_file
@@ -13,8 +14,16 @@ class SuccessCallback(BaseCallback):
     """
     A custom callback that stops training when the agent successfully completes the game.
     """
-    def __init__(self, verbose=0):
+
+    def __init__(self, time_limit: int = 3600, verbose=0):
+        """
+
+        :param time_limit: the max training timme in seconds unit.
+        """
         super(SuccessCallback, self).__init__(verbose)
+        self.start_time = time.time()
+        self.time_limit = time_limit
+        self.is_scheduled = False
 
     def _on_step(self) -> bool:
         """
@@ -24,13 +33,29 @@ class SuccessCallback(BaseCallback):
         dones = self.locals.get('dones')
         infos = self.locals.get('infos')
         if any([infos[i].get('success') if done else False for i, done in enumerate(dones)]):
-            print("Game successfully completed, stopping training...")
+            logging.info("Game successfully completed, stopping training...")
+            self.is_scheduled = True
             return False  # False means "stop training"
+
+        # Check if time limit has been reached
+        elapsed_time = time.time() - self.start_time
+        if elapsed_time > self.time_limit:
+            logging.info(f"Time limit {self.time_limit}s reached, fail to find a solution. Stopping training...")
+            return False  # False means "stop training"
+
         return True  # True means "continue training"
+
+    def get_result(self) -> bool:
+        return self.is_scheduled
 
 
 @timing_decorator(logging.info)
-def schedule(filename: os.PathLike | str, num_envs: int = 1, time_steps: int = 10000):
+def schedule(
+        filename: os.PathLike | str,
+        num_envs: int = 1,
+        time_steps: int = 10000,
+        time_limit: int = 3600
+):
     assert os.path.isfile(filename), f"No such file {filename}"
 
     env = SubprocVecEnv([lambda: from_file(filename) for _ in range(num_envs)])
@@ -41,12 +66,15 @@ def schedule(filename: os.PathLike | str, num_envs: int = 1, time_steps: int = 1
 
     model = MaskablePPO("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1)
 
-    callback = SuccessCallback()
+    callback = SuccessCallback(time_limit=time_limit)
 
     # Train the agent
     model.learn(total_timesteps=time_steps, callback=callback)
 
-    logging.info("------Finish learning------")
+    is_scheduled = callback.get_result()
+    if is_scheduled:
+        logging.info("Successfully scheduling the flows.")
+    else:
+        logging.error("Fail to find a valid solution.")
 
-    return True
-
+    return is_scheduled
