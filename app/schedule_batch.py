cd6135e27d5aef5a1fb4bcdc0d9bdfc3b1a636a0
linjsh <linjsh@pcl.ac.cn>
2023-07-12 20:51:04 +0800

    add schedule batch

---
+++ app/schedule_batch.py
@@ -0,0 +1,34 @@
+import argparse
+import glob
+import logging
+import os
+import pandas as pd
+import time
+from app.scheduler import schedule
+from definitions import OUT_DIR
+
+
+def schedule_batch(files: list[str]):
+    times = []
+    scheduled = []
+    for file in files:
+        start_time = time.time()
+        scheduled.append(schedule(file))
+        end_time = time.time()
+        times.append(end_time - start_time)
+
+    df = pd.DataFrame({"file": files, "time": times, "is_scheduled": scheduled})
+    df.to_csv(os.path.join(OUT_DIR, 'schedule_batch.csv'))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', required=True, type=str)
+    args = parser.parse_args()
+
+    assert os.path.isdir(args.dir), f"No such directory {args.dir}"
+
+    files = glob.glob(os.path.join(args.dir, '*.json'))
+
+    logging.basicConfig(level=logging.INFO)
+    schedule_batch(files)
