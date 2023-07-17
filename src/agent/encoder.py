77e979ee8102d71bc030824de753503cd64a1145
linjsh <linjsh@pcl.ac.cn>
2023-07-17 14:53:32 +0800

    convert dict to batch

--- src/agent/encoder.py
+++ src/agent/encoder.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch_geometric.data import Data, Batch
 
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
@@ -37,6 +38,23 @@ class GNNModel(nn.Module):
         return node_feats
 
 
+def dict_to_batch(obs):
+    # Convert the dict observations to PyTorch Geometric Data objects
+    data_list = []
+    num_batch = obs['adjacency_matrix'].shape[0]
+    print(num_batch)
+    for i in range(obs['adjacency_matrix'].shape[0]):
+        edge_index = obs['adjacency_matrix'][i]
+        x = obs['features_matrix'][i]
+        data = Data(x=x, edge_index=edge_index)
+        data_list.append(data)
+
+    # Convert the list of Data objects to a Batch
+    batch = Batch.from_data_list(data_list)
+
+    return batch
+
+
 class FeaturesExtractor(BaseFeaturesExtractor):
     def __init__(self, observation_space: gym.spaces.Dict):
         state = observation_space.sample()
@@ -48,5 +66,6 @@ class FeaturesExtractor(BaseFeaturesExtractor):
         self.graph_encoder = GNNModel(num_features, 64, embedding_dim)
 
     def forward(self, observations) -> torch.Tensor:
+        batch = dict_to_batch(observations)
         return self.graph_encoder(observations['adjacency_matrix'],
                                   observations['features_matrix'])
