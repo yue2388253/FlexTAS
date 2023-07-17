07f884007f9c39306b5d601e563183bc4f580ce6
linjsh <linjsh@pcl.ac.cn>
2023-07-17 16:24:20 +0800

    refactoring the script to avoid nested loop

--- app/schedule_comparison.py
+++ app/schedule_comparison.py
@@ -1,3 +1,4 @@
+import itertools
 import logging
 import os
 import pandas as pd
@@ -26,32 +27,31 @@ def main(num_flows: str, num_tests: int, seed: int = None, time_limit: int = 300
         num_flows = list(map(int, num_flows.split(',')))
 
     results = []
-    for num_flow in num_flows:
-        for i in range(num_tests):
-            # generate graph and flows.
-            graph = generate_linear_5()
-            flows = generate_flows(graph, num_flow, seed=seed + i)
+    for num_flow, i in itertools.product(num_flows, range(num_tests)):
+        # generate graph and flows.
+        graph = generate_linear_5()
+        flows = generate_flows(graph, num_flow, seed=seed + i)
 
-            # use smt to schedule
-            logging.debug("using smt to schedule...")
-            is_scheduled_smt, smt_time = schedule(graph, flows, SmtScheduler, time_limit)
+        # use smt to schedule
+        logging.debug("using smt to schedule...")
+        is_scheduled_smt, smt_time = schedule(graph, flows, SmtScheduler, time_limit)
 
-            # use drl to schedule
-            logging.debug("using drl to schedule...")
-            is_scheduled_drl, drl_time = schedule(graph, flows, DrlScheduler, time_limit)
+        # use drl to schedule
+        logging.debug("using drl to schedule...")
+        is_scheduled_drl, drl_time = schedule(graph, flows, DrlScheduler, time_limit)
 
-            ratio_drl2smt = drl_time / smt_time
+        ratio_drl2smt = drl_time / smt_time
 
-            results.append([num_flow, i, seed + i,
-                            is_scheduled_smt, smt_time, is_scheduled_drl, drl_time,
-                            ratio_drl2smt])
+        results.append([num_flow, i, seed + i,
+                        is_scheduled_smt, smt_time, is_scheduled_drl, drl_time,
+                        ratio_drl2smt])
 
-        df = pd.DataFrame(results,
-                          columns=['num_flows', 'index', 'seed',
-                                   'smt_scheduled', 'smt_time', 'drl_scheduled', 'drl_time', 'ratio_drl2smt'])
-        filename = os.path.join(OUT_DIR, f'schedule_stat_{num_tests}_{seed}_{time_limit}.csv')
-        df.to_csv(filename)
-        logging.info(f"scheduling statistics is saved to {filename}")
+    df = pd.DataFrame(results,
+                      columns=['num_flows', 'index', 'seed',
+                               'smt_scheduled', 'smt_time', 'drl_scheduled', 'drl_time', 'ratio_drl2smt'])
+    filename = os.path.join(OUT_DIR, f'schedule_stat_{num_tests}_{seed}_{time_limit}.csv')
+    df.to_csv(filename)
+    logging.info(f"scheduling statistics is saved to {filename}")
 
     return
 
