9b1548fc9a75b1f7a01a33a5d9308db278569ce2
linjsh <linjsh@pcl.ac.cn>
2023-07-19 11:16:17 +0800

    refactoring the script

--- app/schedule_comparison.py
+++ app/schedule_comparison.py
@@ -21,66 +21,66 @@ def schedule(scheduler):
     return is_scheduled, elapsed_time
 
 
-def main(num_flows: str, num_tests: int, best_model: str,
-         seed: int = None, time_limit: int = 300, topo: str = 'L5',
-         ):
-    seed = seed if seed is not None else random.randint(0, 10000)
-    if isinstance(num_flows, str):
-        num_flows = list(map(int, num_flows.split(',')))
+def get_graph(topo):
+    if topo == 'L5':
+        return generate_linear_5()
+    elif topo == 'CEV':
+        return generate_cev()
+    raise ValueError(f"Unknown graph type {topo}")
 
-    topos = list(topo.split(','))
 
-    results = []
+def main(num_flows: str, num_tests: int, best_model: str, seed: int = None, time_limit: int = 300, topo: str = 'L5'):
+    seed = seed or random.randint(0, 10000)
+    num_flows = map(int, num_flows.split(',')) if isinstance(num_flows, str) else num_flows
+    topos = topo.split(',')
+
+    filename = os.path.join(OUT_DIR, f'schedule_stat_{topos}_{num_tests}_{seed}_{time_limit}.csv')
+    column_names = ['topo', 'num_flows', 'index', 'seed',
+                    'smt_scheduled', 'smt_time',
+                    'smt_scheduled_no_wait', 'smt_time_no_wait',
+                    'drl_scheduled', 'drl_time',
+                    'ratio_drl2smt']
+    df = pd.DataFrame(columns=column_names)
+
     for topo, num_flow, i in itertools.product(topos, num_flows, range(num_tests)):
-        # generate graph and flows.
-        if topo == 'L5':
-            graph = generate_linear_5()
-        elif topo == 'CEV':
-            graph = generate_cev()
-        else:
-            raise ValueError(f"Unknown graph type {topo}")
+        result = run_tests(topo, num_flow, i, best_model, time_limit, seed)
 
-        logging.info(f"scheduling instance: {topo}, {num_flow} flows, {i}-th")
+        # continuously write the results to avoid losing the results in case the process gets terminated unexpectedly.
+        df_temp = pd.DataFrame([result], columns=column_names)
+        df = pd.concat([df, df_temp], ignore_index=True)
+        df.to_csv(filename, index=False)
 
-        flows = generate_flows(graph, num_flow, seed=seed + i)
-
-        # use smt to schedule
-        logging.info("using smt to schedule...")
-        scheduler = SmtScheduler(graph, flows, timeout_s=time_limit)
-        is_scheduled_smt, smt_time = schedule(scheduler)
-
-        # use no-wait smt to schedule
-        logging.info("using no-wait smt to schedule...")
-        scheduler = NoWaitSmtScheduler(graph, flows, timeout_s=time_limit)
-        is_scheduled_smt_no_wait, smt_no_wait_time = schedule(scheduler)
-
-        # use drl to schedule
-        logging.info("using drl to schedule...")
-        alg, num_envs = re.search(r"_([^_]+)_(\d+)(\.zip)?$", os.path.basename(best_model)).group(1, 2)
-        scheduler = DrlScheduler(graph, flows, time_limit, num_envs=int(num_envs))
-        scheduler.load_model(best_model, alg=alg)
-
-        is_scheduled_drl, drl_time = schedule(scheduler)
-
-        ratio_drl2smt = drl_time / smt_time
-
-        results.append([num_flow, i, seed + i,
-                        is_scheduled_smt, smt_time,
-                        is_scheduled_smt_no_wait, smt_no_wait_time,
-                        is_scheduled_drl, drl_time,
-                        ratio_drl2smt])
-
-    df = pd.DataFrame(results,
-                      columns=['num_flows', 'index', 'seed',
-                               'smt_scheduled', 'smt_time',
-                               'smt_scheduled_no_wait', 'smt_time_no_wait',
-                               'drl_scheduled', 'drl_time',
-                               'ratio_drl2smt'])
-    filename = os.path.join(OUT_DIR, f'schedule_stat_{num_tests}_{seed}_{time_limit}.csv')
-    df.to_csv(filename)
     logging.info(f"scheduling statistics is saved to {filename}")
 
-    return
+
+def run_tests(topo, num_flow, i, best_model, time_limit, seed):
+    graph = get_graph(topo)
+    logging.info(f"scheduling instance: {topo}, {num_flow} flows, {i}-th")
+    flows = generate_flows(graph, num_flow, seed=seed + i)
+
+    schedulers = [
+        ('smt', SmtScheduler(graph, flows, timeout_s=time_limit)),
+        ('no_wait_smt', NoWaitSmtScheduler(graph, flows, timeout_s=time_limit)),
+        ('drl', DrlScheduler(graph, flows, time_limit, num_envs=get_num_envs(best_model)))
+    ]
+    schedulers[-1][1].load_model(best_model, alg=get_alg(best_model))
+
+    results = [topo, num_flow, i, seed + i]
+    for scheduler_str, scheduler in schedulers:
+        logging.info(f"using {scheduler_str} to schedule...")
+        is_scheduled, elapsed_time = schedule(scheduler)
+        results.extend([is_scheduled, elapsed_time])
+
+    results.append(results[-1] / results[-3])  # ratio_drl2smt
+    return results
+
+
+def get_num_envs(best_model):
+    return int(re.search(r"_([^_]+)_(\d+)(\.zip)?$", os.path.basename(best_model)).group(2))
+
+
+def get_alg(best_model):
+    return re.search(r"_([^_]+)_(\d+)(\.zip)?$", os.path.basename(best_model)).group(1)
 
 
 if __name__ == '__main__':
