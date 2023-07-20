15fa4f691e69bdfe2d27de5db02027ebe1cd614f
linjsh <linjsh@pcl.ac.cn>
2023-07-20 17:49:29 +0800

    improve reward function by giving an extra reward when the agent schedules another set of flows.

--- src/env/env.py
+++ src/env/env.py
@@ -130,6 +130,7 @@ class _StateEncoder:
 class NetEnv(gym.Env):
     alpha: float = 1
     beta: float = 10
+    gamma: float = 0.01
 
     def __init__(self, graph: nx.DiGraph = None, flows: list[Flow] = None):
         super().__init__()
@@ -344,13 +345,15 @@ class NetEnv(gym.Env):
             self.flows_scheduled[self.flow_index] = 1
             self.flow_index += 1
 
+            if self.flow_index % int(self.num_flows * 0.1) == 0:
+                # give an extra reward when the agent schedule another set of flows.
+                self.reward += self.gamma * (self.flow_index ** 2)
+
             if all(self.flows_scheduled):
                 # all flows are scheduled.
                 filename = os.path.join(OUT_DIR, f'schedule_rl_{id(self)}.log')
                 self.save_results(filename)
                 self.logger.info(f"Good job! Finish scheduling! Scheduling result is saved at {filename}.")
-
-                self.reward = len(self.flows)
 
                 return self.observation_space.sample(), self.reward, True, False, {'success': True}
 
