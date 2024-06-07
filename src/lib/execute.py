2a5c948d619ccb00cab69b48ab06fc479f2c9662
linjsh <linjsh@pcl.ac.cn>
2024-06-07 17:02:38 +0800

    improve execute from cmd line

--- src/lib/execute.py
+++ src/lib/execute.py
@@ -1,8 +1,28 @@
 import argparse
 import inspect
+from typing import Any, Callable, List, Type, get_origin, get_args
 
 
-def execute_from_command_line(func):
+def custom_list_type(item_type: Type) -> Callable[[str], List[Any]]:
+    """
+    Generates a function to convert a comma-separated string into a list of a specific type.
+
+    Parameters
+    ----------
+    item_type : Type
+        The type of the items in the resulting list.
+
+    Returns
+    -------
+    Callable[[str], List[Any]]
+        A function that converts a string to a list of the specified item type.
+    """
+    def convert(s: str) -> List[Any]:
+        return [item_type(item) for item in s.split(',')]
+    return convert
+
+
+def execute_from_command_line(func: Callable):
     """
     Executes a given function using arguments provided from the command line.
 
@@ -33,11 +53,22 @@ def execute_from_command_line(func):
     sig = inspect.signature(func)
 
     # Create the argument parser
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=func.__doc__)
 
     # Add arguments to the parser
     for name, param in sig.parameters.items():
-        arg_type = param.annotation if param.annotation is not param.empty else str
+        # Determine the type of the argument
+        if param.annotation is not param.empty:
+            arg_type = param.annotation
+            # Check if the annotation is a generic type
+            origin_type = get_origin(arg_type)
+            if origin_type is list:
+                # Get the inner type of the list
+                inner_type = get_args(arg_type)[0]
+                arg_type = custom_list_type(inner_type)
+        else:
+            arg_type = str
+
         if param.default is param.empty:  # it's a required argument
             parser.add_argument('--' + name, required=True, type=arg_type)
         else:  # it's an optional argument, use default value from function definition
@@ -49,4 +80,4 @@ def execute_from_command_line(func):
     args_dict = vars(args)
 
     # Call the function with the arguments
-    func(**args_dict)
+    return func(**args_dict)
